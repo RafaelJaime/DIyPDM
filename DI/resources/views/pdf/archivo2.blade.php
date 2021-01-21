763801262550d9d822dbc19e9621dbbe27150b42
RafaelJaimeMoreno <rajaimor@gmail.com>
2021-01-21 09:31:15 +0100

    vamo a pdf

--- DI/resources/views/pdf/archivo2.blade.php
+++ DI/resources/views/pdf/archivo2.blade.php
@@ -1,25 +1,36 @@
-<h1>SalesIn</h1>
-<h2>Informe sobre los usuarios</h2>
-<img src="{{ asset('logotipo.png') }}" alt="Logotipo del colegio">
-<table border="1" class= "table table-light table-hover default">
-    <thead class="thead-light">
-        <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Cicle_Id</th>
-            <th>Email</th>
-        </tr>
-    </thead>
+<!DOCTYPE html>
+<html lang="en">
 
-    <tbody>
-    @foreach($users as $user)
-        <tr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-            <td>{{$user->name}}</td>
-            <td>{{$user->surname}}</td>
-            <td>{{$user->find($user->id)->cicle->name}}</td>
-            <td>{{$user->email}}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<body>
+    <h1>SalesIn</h1>
+    <h2>Informe sobre los usuarios</h2>
+    <table border="1" class="table table-light table-hover default">
+        <thead class="thead-light">
+            <tr>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Cicle_Id</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{$user->name}}</td>
+                <td>{{$user->surname}}</td>
+                <td>{{$user->find($user->id)->cicle->name}}</td>
+                <td>{{$user->email}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
