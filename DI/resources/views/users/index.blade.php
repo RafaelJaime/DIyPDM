5826eceaaa5cb1fe02c3c4a0e2b95fb5689aa182
RafaelJaimeMoreno <rajaimor@gmail.com>
2021-01-26 10:26:12 +0100

    HTML

--- DI/resources/views/users/index.blade.php
+++ DI/resources/views/users/index.blade.php
@@ -11,12 +11,18 @@ Página principal
         <div class="box" style="border:1px solid #d2d6de;">
             <div class="box-header" style="background-color:#f5f5f5;border-bottom:1px solid #d2d6de;">
                 <div class="form-group">
+                <div class="col-md-6">
                 <label>What users do you want to see? </label>
                 <select id="activate" name="filter" class="form-control">
                     <option value="">All</option>
                     <option value="1">Activate Users</option>
                     <option value="0">Non Activate Users</option>
                 </select>
+                </div>
+                <div class="col-md-6">
+                    AQUI BOTON
+                </div>
+                
                 </div>
             </div>
             <div class="box-body table-responsive no-padding">
