ef3d7b7fdb1b3dde3c4ffb69d9c4c17ca68f99b9
RafaelJaimeMoreno <rajaimor@gmail.com>
2021-02-08 13:32:48 +0100

    cambios raros

--- DI/app/Http/Controllers/emailController.php
+++ DI/app/Http/Controllers/emailController.php
@@ -14,16 +14,21 @@ class emailController extends Controller
     public function envio(Request $request)
     {
         $data = [
-            'emailto' => $request->get('to'),
+            'emailto' => explode(',', $request->get('to')),
             'subject' => $request->get('subject'),
             'content' => $request->get('contenido'),
+            'file' => $request['file'],
         ];
-
         Mail::send('email.vistaEmail', $data, function ($message) use ($data) {
             $message->from('chimpancesjaimecamero@gmail.com');
             $message->to($data['emailto'])->subject($data['subject']);
+            if ($data['file'] != null) {
+                $message->attach($data['file']->getRealPath(), [
+                    'as' => request()->file('file')->getClientOriginalExtension(),
+                    'mime'=> request()->file('file')->getMimeType()]);
+            }
+            
         });
-
         return redirect('completeEmail');
     }
     public function complete()
